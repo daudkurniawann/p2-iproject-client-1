3b529396719abd5d911b0fdfa5fed896b13a2e34
Daud Kurniawan <daudkurniaw@gmail.com>
2022-11-08 13:57:26 +0700

    login spotify with supabase

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid place-items-center h-screen">
+    <button
+      @click.prevent="clickLogin"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      LOGIN
+    </button>
+  </div>
+</template>
+
+<script>
+import { useLoginStore } from "../stores/loginStore";
+import { mapActions } from "pinia";
+export default {
+  name: "login",
+  methods: {
+    ...mapActions(useLoginStore, ["signInWithSpotify"]),
+
+    clickLogin() {
+      this.signInWithSpotify();
+    },
+  },
+};
+</script>
